313cc5373e2fb5a92389269e357e0da63ffef277
ccs19@students.uwf.edu <ccs19@students.uwf.edu>
2015-04-17 16:18:47 -0500

    Added bare class for conversion.

---
+++ src/com/company/YuyvToI420.java
@@ -0,0 +1,28 @@
+package com.company;
+
+/**
+ * Created by Chris on 4/17/2015.
+ */
+public class YuyvToI420 {
+
+    private int mWidth, mHeight;
+
+
+    public YuyvToI420(int width, int height){
+        mWidth = width;
+        mHeight = height;
+    }
+
+    public byte[] convertData(byte[] data){
+        byte[] processedData = new byte[mWidth*mHeight*(3/2)];
+
+
+
+
+
+
+        return processedData;
+    }
+
+
+}
