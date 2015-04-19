7fce800b9b16f264f7fa780c8a48a35ac0858803
ccs19@students.uwf.edu <ccs19@students.uwf.edu>
2015-04-19 18:19:58 -0500

    Successfully converted image!

--- src/com/company/YuyvToI420.java
+++ src/com/company/YuyvToI420.java
@@ -19,47 +19,24 @@ public class YuyvToI420 {
 
     public byte[] convertData(byte[] data){
         //byte[] processedData = new byte[mWidth*mHeight*(3/2)];
-        byte[] uData = new byte[mWidth*mHeight/2];
-        byte[] vData = new byte[mWidth*mHeight/2];
+        byte[] uData = new byte[mWidth*mHeight/4];
+        byte[] vData = new byte[mWidth*mHeight/4];
         byte[] yData = new byte[mWidth*mHeight];
 
-        byte[] uDataAveraged = new byte[mWidth*mHeight/4];
-        byte[] vDataAveraged = new byte[mWidth*mHeight/4];
-
         int yDataIndex = 0, uDataIndex = 0, vDataIndex = 0;
+        int index = 0;
 
-        for(int index = 0; index < mHeight*mWidth; index++){
-            yData[yDataIndex++] = data[index*2];
-
-            if(index%2 == 0){
-                try {
-                    uData[uDataIndex++] = data[index * 2 + 1];
-                    vData[vDataIndex++] = data[index * 2 + 3];
-                }catch(Exception e){
-
+        for(int y = 0; y < mHeight; y++){
+            for(int x = 0; x < mWidth; x++) {
+                index = (x + y*mWidth)*2;
+                System.out.println(index);
+                yData[yDataIndex++] = data[index];
+                if (y % 2 == 0 && x%2 == 0) {
+                    uData[uDataIndex++] = clip(data[index + 1], data[index + 1 + 4]);
+                    vData[vDataIndex++] = clip(data[index + 3], data[index + 3 + 4]);
                 }
             }
         }
-        System.out.println("index " + uDataIndex);
-
-
-        //Parse UV data
-        int uAvgInd = 0;
-        int vAvgInd = 0;
-        int rowOffset = 1;
-        for(int x = 0; x < uData.length/2; x++) {
-            byte uDataOne, uDataTwo, vDataOne, vDataTwo;
-            if(x%(mWidth/2*rowOffset) == 0 && x>=(mWidth/2*rowOffset))rowOffset++;
-            uDataOne = (uData[x+(mWidth/2)*(rowOffset-1)]);
-            uDataTwo = (uData[x+(mWidth/2)*(rowOffset-1) + (mWidth/2)]);
-            vDataOne = (vData[x+(mWidth/2)*(rowOffset-1)]);
-            vDataTwo = (vData[x+(mWidth/2)*(rowOffset-1) + (mWidth/2)]);
-
-            uDataAveraged[uAvgInd++] = clip(uDataOne, uDataTwo);
-            vDataAveraged[vAvgInd++] = clip(vDataOne, vDataTwo);
-        }
-
-        System.out.println("index " + vAvgInd);
 
 
 
@@ -67,8 +44,8 @@ public class YuyvToI420 {
         ByteArrayOutputStream processedData = new ByteArrayOutputStream();
         try {
             processedData.write(yData);
-            processedData.write(uDataAveraged);
-            processedData.write(vDataAveraged);
+            processedData.write(uData);
+            processedData.write(vData);
         }catch(IOException e){
             System.err.println("Failed to write to ByteArrayOutputStream " + e);
         }
@@ -78,10 +55,12 @@ public class YuyvToI420 {
 
 
     private byte clip(byte one, byte two){
-        int result = (one+two)/2;
-        System.out.println(result&0xff);
-        if((result&0xff) > 255) return Byte.MAX_VALUE;
-        else if((result&0xff) < 0) return Byte.MIN_VALUE;
+        int byteOne = Byte.toUnsignedInt(one);
+        int byteTwo = Byte.toUnsignedInt(two);
+        int result = (byteOne+byteTwo)/2;
+        //System.out.println(result&0xff);
+        if(result > 255) return Byte.MAX_VALUE;
+        else if(result < 0) return Byte.MIN_VALUE;
         else return (byte)(result&0xff);
     }
 
