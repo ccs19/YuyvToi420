d052c5f0d094761d53b0b74b81583d37d9001937
ccs19@students.uwf.edu <ccs19@students.uwf.edu>
2015-04-19 16:37:16 -0500

    Wrote code to split up byte array and return parsed data

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,8 @@ package com.company;
 
 public class Main {
 
+    static int width = 176, height = 144;
+
     public static void main(String[] args) {
 	// write your code here
     }
--- src/com/company/YuyvToI420.java
+++ src/com/company/YuyvToI420.java
@@ -1,5 +1,9 @@
 package com.company;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 /**
  * Created by Chris on 4/17/2015.
  */
@@ -14,15 +18,32 @@ public class YuyvToI420 {
     }
 
     public byte[] convertData(byte[] data){
-        byte[] processedData = new byte[mWidth*mHeight*(3/2)];
+        //byte[] processedData = new byte[mWidth*mHeight*(3/2)];
         byte[] uData = new byte[mWidth*mHeight/4];
         byte[] vData = new byte[mWidth*mHeight/4];
         byte[] yData = new byte[mWidth*mHeight];
 
+        int yDataIndex = 0, uDataIndex = 0, vDataIndex = 0;
 
+        for(int index = 0; index < mHeight*mWidth; index++){
+            yData[yDataIndex++] = data[index*2];
 
+            if(index%8 == 0){
+                uData[uDataIndex++] = (byte)(((data[index*2+1] + data[index*2+5])/2)&0xff);
+                vData[vDataIndex++] = (byte)(((data[index*2+3] + data[index*2+7])/2)&0xff);
+            }
 
-        return processedData;
+        }
+        ByteArrayOutputStream processedData = new ByteArrayOutputStream();
+        try {
+            processedData.write(yData);
+            processedData.write(uData);
+            processedData.write(vData);
+        }catch(IOException e){
+            System.err.println("Failed to write to ByteArrayOutputStream " + e);
+        }
+
+        return processedData.toByteArray();
     }
 
 
