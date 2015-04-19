5952cc8253b4dd8f9891dce7af9117d507ec65cc
ccs19@students.uwf.edu <ccs19@students.uwf.edu>
2015-04-19 17:19:54 -0500

    Ugly code written to test u/v data. Pictures not coming out correctly, so trying different things.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,41 @@
 package com.company;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
 
     static int width = 176, height = 144;
 
     public static void main(String[] args) {
-	// write your code here
+	    Path filePath = Paths.get("C:\\Users\\Chris\\IdeaProjects\\YuyvToi420\\sample.yuv");
+        byte[] imageData = null, dataResult = null;
+
+
+        try {
+            imageData = Files.readAllBytes(filePath);
+        }catch(Exception e){
+            System.err.println("Failed to read file " + e);
+        }
+
+        if(imageData != null) {
+            YuyvToI420 processor = new YuyvToI420(width, height);
+            dataResult = processor.convertData(imageData);
+        }
+
+
+        try {
+            FileOutputStream saveResult = new FileOutputStream(new File("convertedImage.yuv"));
+            saveResult.write(dataResult);
+
+        }catch(Exception e ){
+            System.err.println("Failed to open outputstream and write data " + e);
+        }
+
+
+
     }
 }
--- src/com/company/YuyvToI420.java
+++ src/com/company/YuyvToI420.java
@@ -19,26 +19,55 @@ public class YuyvToI420 {
 
     public byte[] convertData(byte[] data){
         //byte[] processedData = new byte[mWidth*mHeight*(3/2)];
-        byte[] uData = new byte[mWidth*mHeight/4];
-        byte[] vData = new byte[mWidth*mHeight/4];
+        byte[] uData = new byte[mWidth*mHeight/2];
+        byte[] vData = new byte[mWidth*mHeight/2];
         byte[] yData = new byte[mWidth*mHeight];
+
+        byte[] uDataAveraged = new byte[mWidth*mHeight/2];
+        byte[] vDataAveraged = new byte[mWidth*mHeight/2];
 
         int yDataIndex = 0, uDataIndex = 0, vDataIndex = 0;
 
         for(int index = 0; index < mHeight*mWidth; index++){
             yData[yDataIndex++] = data[index*2];
 
-            if(index%8 == 0){
-                uData[uDataIndex++] = (byte)(((data[index*2+1] + data[index*2+5])/2)&0xff);
-                vData[vDataIndex++] = (byte)(((data[index*2+3] + data[index*2+7])/2)&0xff);
-            }
+            if(index%2 == 0){
+                try {
+                    uData[uDataIndex++] = data[index * 2 + 1];
+                    vData[vDataIndex++] = data[index * 2 + 3];
+                }catch(Exception e){
 
+                }
+            }
         }
+
+
+        //Parse UV data
+        int uAvgInd = 0;
+        int vAvgInd = 0;
+        for(int x = 0; x < mWidth/2 ; x++){
+            for(int y = 0; y < mHeight/2; y++){
+                byte uDataOne, uDataTwo, vDataOne, vDataTwo;
+                uDataOne = uData[y*mWidth + x];
+                uDataTwo = uData[y*mWidth+(mWidth/2)+x];
+                vDataOne = vData[y*mWidth + x];
+                vDataTwo = vData[y*mWidth+(mWidth/2)+x];
+
+                uDataAveraged[uAvgInd++] = (byte)(((uDataOne+uDataTwo)/2)&0xff);
+                vDataAveraged[vAvgInd++] = (byte)(((vDataOne+vDataTwo)/2)&0xff);
+            }
+        }
+
+
+
+
+
+
         ByteArrayOutputStream processedData = new ByteArrayOutputStream();
         try {
             processedData.write(yData);
-            processedData.write(uData);
-            processedData.write(vData);
+            processedData.write(uDataAveraged);
+            processedData.write(vDataAveraged);
         }catch(IOException e){
             System.err.println("Failed to write to ByteArrayOutputStream " + e);
         }
