d9ac377c231a77ef07e47da579cfdf8a0ed876b8
ccs19@students.uwf.edu <ccs19@students.uwf.edu>
2015-04-19 19:08:23 -0500

    Converted clip() to one liner.
    Note to self: Signed bytes suck.

--- src/com/company/YuyvToI420.java
+++ src/com/company/YuyvToI420.java
@@ -1,40 +1,43 @@
 package com.company;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-
 /**
- * Created by Chris on 4/17/2015.
+ * Converts a YUY2 frame to I420
  */
 public class YuyvToI420 {
 
     private int mWidth, mHeight;
 
 
+    /**
+     * Constructor for class
+     * @param width Frame width
+     * @param height Frame height
+     */
     public YuyvToI420(int width, int height){
         mWidth = width;
         mHeight = height;
     }
 
+    /**
+     * Converts a single YUY2 frame to I420
+     * @param data byte array of YUY2 data
+     * @return I420 Frame
+     */
     public byte[] convertData(byte[] data){
         byte[] processedDataByte = new byte[mWidth*mHeight + (mWidth*mHeight/2)];
-
         int uIndexOffset = mWidth*mHeight;
-        int uvSize = mWidth*mHeight/4;
+        int uvArraySize = mWidth*mHeight/4;
+        int dataIndex;
 
-        int index;
-
-        for(int y = 0; y < mHeight; y++){
-            for(int x = 0; x < mWidth; x++) {
-                index = (x + y * mWidth) * 2;
-                processedDataByte[x + mWidth*y] = data[index];
-                if (y % 2 == 0 && x%2 == 0) {
+        for(int y = 0; y < mHeight; y++){ //Y plane
+            for(int x = 0; x < mWidth; x++) { //X Plane
+                dataIndex = (x + y * mWidth) * 2;
+                processedDataByte[x + mWidth * y] = data[dataIndex];
+                if (y % 2 == 0 && x % 2 == 0) {
                     int uIndex = uIndexOffset + x/2 + y*(mWidth/4);
-                    int vIndex = uIndex + uvSize ;
-                    //System.out.printf("index u/v/max %d %d %d\n", uIndex, vIndex, processedDataByte.length);
-                    processedDataByte[uIndex] = clip(data[index + 1], data[index + 1 + 4]);
-                    processedDataByte[vIndex] = clip(data[index + 3], data[index + 3 + 4]);
+                    int vIndex = uIndex + uvArraySize ;
+                    processedDataByte[uIndex] = clip(data[dataIndex + 1], data[dataIndex + 1 + 4]);
+                    processedDataByte[vIndex] = clip(data[dataIndex + 3], data[dataIndex + 3 + 4]);
                 }
             }
         }
@@ -43,16 +46,7 @@ public class YuyvToI420 {
 
 
     private byte clip(byte one, byte two){
-        int byteOne = Byte.toUnsignedInt(one);
-        int byteTwo = Byte.toUnsignedInt(two);
-        int result = (byteOne+byteTwo)/2;
-        if(result > 255) return Byte.MAX_VALUE;
-        else if(result < 0) return Byte.MIN_VALUE;
-        else return (byte)(result&0xff);
+        return (byte)
+                (((Byte.toUnsignedInt(one) + Byte.toUnsignedInt(two))/2)&0xff);
     }
-
-
-
-
-
 }
